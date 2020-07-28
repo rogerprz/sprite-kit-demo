8196505a2e2324b0a8cbfe8b1bfdab2030e8348e
rogerprz <rogerprz@github.com>
2020-07-27 23:30:31 -0700

    removed gaveviewController code

--- sprite-kit-demo/GameViewController.swift
+++ sprite-kit-demo/GameViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
@@ -30,21 +29,5 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
     }
 }
