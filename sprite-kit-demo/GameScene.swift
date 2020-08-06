38ede249b37f76f76dcf91ad010300767f446d40
rogerprz <rogerprz@github.com>
2020-08-05 23:17:08 -0700

    add blue sprite node to red

--- sprite-kit-demo/GameScene.swift
+++ sprite-kit-demo/GameScene.swift
@@ -12,6 +12,8 @@ class GameScene: SKScene {
     
     let myFirstNode = SKNode()
     let myFirstSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200.0, height: 200.0))
+    let myFirstTexturedSpriteNode = SKSpriteNode(imageNamed: "Spaceship")
+    let blueSpriteNode = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100.0, height: 100.0))
     
     override func didMove(to view: SKView) {
         addChild(myFirstNode)
@@ -19,5 +21,13 @@ class GameScene: SKScene {
         myFirstSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
         myFirstSpriteNode.anchorPoint = CGPoint.zero
         addChild(myFirstSpriteNode)
+        
+        myFirstTexturedSpriteNode.zPosition = 1
+        myFirstTexturedSpriteNode.size = CGSize(width: 100.0, height: 100.0)
+        myFirstSpriteNode.addChild(myFirstTexturedSpriteNode)
+        
+        blueSpriteNode.position = CGPoint(x: myFirstSpriteNode.size.width/2, y: myFirstSpriteNode.size.width/2)
+        blueSpriteNode.zPosition = 2
+        myFirstSpriteNode.addChild(blueSpriteNode)
     }
 }
