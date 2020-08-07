a4bdede9f4452cadbcc07482ff335f31b908a4ef
rogerprz <rogerprz@github.com>
2020-08-07 15:13:35 -0700

    commented out prev ex

--- sprite-kit-demo/GameScene.swift
+++ sprite-kit-demo/GameScene.swift
@@ -29,27 +29,33 @@ class GameScene: SKScene {
         blueBox.position = CGPoint(x: myFirstSpriteNode.size.width/2, y: myFirstSpriteNode.size.width/2)
         blueBox.zPosition = 2
         myFirstSpriteNode.addChild(blueBox)
+        
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0))
-//        Roates half and then stops
-//        blueBox.run(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0))
-//        Never stops
-        if !blueBox.hasActions(){
-//            blueBox.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0)))
-//            blueBox.run(SKAction.group([SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0), SKAction.scale(by:0.7, duration: 1.0)]))
-            blueBox.run(SKAction.sequence([SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0), SKAction.scale(by:0.7, duration: 1.0)]))
-        } else {
-            blueBox.removeAllActions()
-        }
-
-        
-        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0)){
-            self.myFirstTexturedSpriteNode.position = CGPoint.zero
-        }
-        
-    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+////        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0))
+////        Roates half and then stops
+////        blueBox.run(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0))
+////        Never stops
+////        if !blueBox.hasActions(){
+//////            blueBox.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0)))
+//////            blueBox.run(SKAction.group([SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0), SKAction.scale(by:0.7, duration: 1.0)]))
+////            blueBox.run(SKAction.sequence([SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0), SKAction.scale(by:0.7, duration: 1.0)]))
+////        } else {
+////            blueBox.removeAllActions()
+////        }
+////
+////
+////        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0)){
+////            self.myFirstTexturedSpriteNode.position = CGPoint.zero
+////        }
+//        if let _ = myFirstTexturedSpriteNode.action(forKey: "Rotation"){
+//            myFirstTexturedSpriteNode.removeAction(forKey: "Rotation")
+//        } else {
+//            myFirstTexturedSpriteNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1.0)),withKey: "Rotation")
+//        }
+//
+//    }
     
     
 }
