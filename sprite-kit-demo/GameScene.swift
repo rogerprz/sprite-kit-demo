ff3b4ef54aef8c440bb4d6e5684fa6f6de0fab20
rogerprz <rogerprz@github.com>
2020-08-10 22:37:00 -0700

    commented out code

--- sprite-kit-demo/GameScene.swift
+++ sprite-kit-demo/GameScene.swift
@@ -10,31 +10,35 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    override func didMove(to view: SKView) {
+        
+    }
+    
     let dogSpriteNode = SKSpriteNode(imageNamed: "Run0")
     var dogFrames = [SKTexture]()
    
-    override func didMove(to view: SKView) {
-        dogSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
-        addChild(dogSpriteNode)
-        
-        let texturedAtlas = SKTextureAtlas(named: "Dog Frames")
-        
-        for index in 0..<texturedAtlas.textureNames.count {
-            let textureName = "Run" + String(index)
-            dogFrames.append(texturedAtlas.textureNamed((textureName)))
-        }
-       
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if (dogSpriteNode.hasActions()){
-            dogSpriteNode.removeAllActions()
-        } else {
-            dogSpriteNode.run(SKAction.repeatForever(SKAction.animate(with: dogFrames, timePerFrame: 0.1)))
-        }
-        
-        
-    }
+//    override func didMove(to view: SKView) {
+//        dogSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+//        addChild(dogSpriteNode)
+//        
+//        let texturedAtlas = SKTextureAtlas(named: "Dog Frames")
+//        
+//        for index in 0..<texturedAtlas.textureNames.count {
+//            let textureName = "Run" + String(index)
+//            dogFrames.append(texturedAtlas.textureNamed((textureName)))
+//        }
+//       
+//    }
+//    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if (dogSpriteNode.hasActions()){
+//            dogSpriteNode.removeAllActions()
+//        } else {
+//            dogSpriteNode.run(SKAction.repeatForever(SKAction.animate(with: dogFrames, timePerFrame: 0.1)))
+//        }
+//        
+//        
+//    }
     
 
     
